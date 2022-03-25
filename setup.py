d377d0f22c7cdbe968dcdbe9d11bbb8e1c6d8b0d
David J. Cox <cox.david.j@gmail.com>
2022-03-25 06:38:17 -0400

    setup repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setup tools import find_packages, setup
+
+setup(
+	name='src', 
+	packages=find_packages(), 
+	versions='0.1.0', 
+	description='FILL ME IN WITH A PROJECT DESCRIPTION', 
+	author='FILL ME IN WITH YOUR NAME', 
+	license=None
+)
